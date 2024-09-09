a862cda70dd2687b790cb8f1b6ba11dd4311310f
AvigailH <154588429+AvigailH@users.noreply.github.com>
2024-09-09 17:37:03 +0300

    update file

--- middelwares/autorition.js
+++ middelwares/autorition.js
@@ -1,25 +1,28 @@
 const jwt = require("jsonwebtoken");
 const user = require("../models/modelUser");
-const secret = "123$#%12D";
+
+const secret = process.env.JWT_SECRET;
 
 function createToken(req, res, next) {
   console.log("arrive func create token");
   const { name, password, email } = req.body;
-  const token = jwt.sign({ name: name, password: password ,email: email }, secret);
-  //   res.send({accessToken: token})
+  const token = jwt.sign({ name: name, password: password, email: email }, secret);
   res.accessToken = token;
   next();
 }
 
-async function auth (req, res, next) {
+async function auth(req, res, next) {
   try {
     const token = req.headers["authorization"];
+    if (!token) {
+      return res.status(401).send("No token provided");
+    }
     const decoded = jwt.verify(token, secret);
     const user2 = await user.find({ password: decoded.password });
-    req.userId =user2.userId
-     next();
-  } catch {
-    res.status(401).send("unautorizied");
+    req.userId = user2.userId;
+    next();
+  } catch (error) {
+    res.status(401).send("Unauthorized");
   }
 }
 
